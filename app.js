bb9a44ff7da981b9ec03101888cde1dbc56b032b
monti242421 <monti242421@gmail.com>
2023-09-21 02:03:28 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-app.use((req,res,next)=>{
-    console.log("In the MiddleWare");
-    next(); //allows the request to contnue to next middleware in line
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<h1><form action="/product" method="POST"><Input type "text" name = "title"><Input type "text" name = "size"><button type="submit">Submit</button> </form></h1>');
 });
 
-app.use((req,res,next)=>{
-    console.log("In another MiddleWare");
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req,res,next)=>{
     res.send('<h1>Hello from Express</h1>');
 });
 
