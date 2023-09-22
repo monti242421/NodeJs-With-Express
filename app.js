7235399940917a1ac99d65006b8c21c50e28be61
monti242421 <monti242421@gmail.com>
2023-09-22 15:08:53 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,21 +2,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require("path");
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')))
 
-app.use('/add-product',(req,res,next)=>{
-    res.send('<h1><form action="/product" method="POST"><Input type "text" name = "title"><Input type "text" name = "size"><button type="submit">Submit</button> </form></h1>');
-});
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+})
 
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Hello from Express</h1>');
-});
 
 app.listen(4000);
 
