ab1f20a25e5bf315491604d26c98a9288cb8e6f1
monti242421 <monti242421@gmail.com>
2023-09-23 20:00:57 +0530

    Add files via upload

---
+++ database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'fzr2000o'
+})
+
+module.exports = pool.promise();
